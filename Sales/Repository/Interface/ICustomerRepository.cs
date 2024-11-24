c1c387fbaa446b44aaa17bd9b88d443665bf4cda
bis29abena <bismarkoseiabena@gmail.com>
2024-11-24 14:46:19 +0000

    added repository

---
+++ Sales/Repository/Interface/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sales.Models;
+
+namespace Sales.Repository.Interface
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetAll();
+        Customer GetCustomer(int id);
+        void UpdateCustomer(Customer customer);
+        void AddCustomer(Customer customer);
+        void DeleteCustomer(int id);
+    }
+}
